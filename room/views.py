c438a67f5246c9d504d0961f789ac1ed7c779122
Arman170616 <armanice09@gmail.com>
2022-06-18 18:22:10 +0600

    room views

--- room/views.py
+++ room/views.py
@@ -1,3 +1,12 @@
+import re
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Room
+
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms':rooms})
