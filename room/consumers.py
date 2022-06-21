bf5327250d73ce8b3ca1c512aac41198ceaac638
Arman170616 <armanice09@gmail.com>
2022-06-22 02:05:15 +0600

    async chat message

--- room/consumers.py
+++ room/consumers.py
@@ -1,3 +1,4 @@
+from email import message
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -21,4 +22,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-        
+    
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': username,
+                'room':room,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+
+        await  self.send(text_data = json.dumps({
+            'message': message,
+            'username': username,
+            'room': room,
+        }))
